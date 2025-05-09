f00e0ac91d85483510cdb1dc3864ed65348bea54
Jay <166980354+Jayraj1202@users.noreply.github.com>
2025-05-09 09:13:08 +0530

    Add files via upload

---
+++ Organic Fram/js/script.js
@@ -0,0 +1,8 @@
+function showOffers() {
+  const offerText = `
+    🌿 10% off on all green vegetables!<br>
+    🍎 Buy 2kg fruits, get 1kg free!<br>
+    🧺 Visit the farm and get a free organic basket!
+  `;
+  document.getElementById("offerDisplay").innerHTML = offerText;
+}
---
+++ Renewable Energy/js/script.js
@@ -0,0 +1,8 @@
+function displayOffers() {
+  const offers = `
+    ☀️ 20% off on residential solar panels installation!<br>
+    🔋 Free battery check-up with every system purchase!<br>
+    🛠️ First-year maintenance service absolutely free!
+  `;
+  document.getElementById("offerDisplay").innerHTML = offers;
+}
